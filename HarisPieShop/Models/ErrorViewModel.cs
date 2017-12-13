9a5bd3652e93024229c4c6e1f4d8ee3befb2e1e8
Haris Lilic <lharis16@live.com>
2017-12-12 22:08:32 -0600

    Add project files.

---
+++ HarisPieShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HarisPieShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
